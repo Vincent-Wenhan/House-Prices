6c7798fdd957a303e9d1640b9fc355d1915af692
Vincent <jiangwenhan@westlake.edu.cn>
2026-03-03 15:51:52 +0800

    model

---
+++ src/model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+
+class MLP(nn.Module):
+    "A simple multi-layer perceptron model for regression tasks."
+    def __init__(self, cfg):
+        super().__init__()
+        layers = []
+        input_size = cfg.model.input_size
+        for hidden_size in cfg.model.hidden_sizes:
+            layers.append(nn.Linear(input_size, hidden_size))
+            layers.append(cfg.model.activation())
+            layers.append(nn.Dropout(cfg.model.dropout))
+            input_size = hidden_size
+        layers.append(nn.Linear(input_size, cfg.model.output_size))
+        self.net = nn.Sequential(*layers)
+        
+    def forward(self, x):
+        return self.net(x)
+    
+
+
